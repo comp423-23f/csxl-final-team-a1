1fbf34e9c50f25a346db3eecb249af89125df828
Benjamin Eldridge <97560111+benjabenn@users.noreply.github.com>
2023-11-15 18:25:43 -0500

    Equipment type backend routes (#46)
    * Refactor equipment api and add admin api file
    
    The equipment.py file that existed previously has been renamed
    and moved into a new directory to align with the specifications
    of the design doc's apis. equipment_admin has been started to
    include routes for adding, modifying, and deleting equipment
    types.
    
    * Begin skeleton route implementation
    
    * Add routes and service layer for add, edit, delete
    
    The routes and the service layer for the admin to add, modify,
    and delete equipment types have been added and connected together.
    
    * Update documentation and integrate PR improvements
    
    The documentation for the parameters/args of service methods
    and api routes have begun to be updated and suggested changes
    to permission strings and return statements.
    
    * Finish args documentation update equipment service

--- backend/api/equipment/equipment_admin.py
+++ backend/api/equipment/equipment_admin.py
@@ -1,44 +1,76 @@
 from fastapi import APIRouter, Depends
-from ..authentication import authenticated_pid
+
+from backend.models.user import User
+from ..authentication import authenticated_pid, registered_user
 from ...services.equipment import EquipmentService, EquipmentType, EquipmentItem
 from ...models.equipment.equipment_type import EquipmentType
 from ...models.equipment.equipment_item import EquipmentItem
 from ...models.equipment.item_details import ItemDetails
 from ...models.equipment.type_details import TypeDetails
 
-api = APIRouter(prefix="/api/equipment/admin")
+api = APIRouter(prefix="/api/equipment")
 openapi_tags = {
-    "name": "Equipment Reservation System Admin",
+    "name": "Admin Equipment Reservation System",
     "description": "Admin-only system for adding, modifying, and deleting equipment from the system.",
 }
 
-@api.post("/add-equipment-type", tags=["Equipment Reservation System Admin"])
-def add_equipment_type(equipment_type: EquipmentType, pid_onyen: tuple[int, str] = Depends(authenticated_pid)) -> TypeDetails:
+# NOTE: Make sure to add tags to all subsequent api calls for them to show in /docs
+
+
+@api.post("/add-equipment-type", tags=["Admin Equipment Reservation System"])
+def add_equipment_type(
+    equipment_type: EquipmentType,
+    subject: User = Depends(registered_user),
+    equipment_service: EquipmentService = Depends(),
+) -> EquipmentType:
     """
     Adds a new EquipmentType to the system
 
+    Parameters:
+        equipment_type (EquipmentType): The new EquipmentType to add
+        subject (User): The user attempting the action
+        equipment_service (EquipmentService): The Equipment backend service layer
     Returns:
-        EquipmentType - the Equipment Type that was just added
+        EquipmentType: The Equipment Type that was just added
     """
-    ...
+    return equipment_service.add_equipment_type(subject, equipment_type)
 
 
-@api.put("/modify-equipment-type", tags=["Equipment Reservation System Admin"])
-def modify_equipment_type(equipment_type: EquipmentType, pid_onyen: tuple[int, str] = Depends(authenticated_pid)) -> EquipmentType:
+@api.put("/modify-equipment-type", tags=["Admin Equipment Reservation System"])
+def modify_equipment_type(
+    equipment_type: EquipmentType,
+    id: int,
+    subject: User = Depends(registered_user),
+    equipment_service: EquipmentService = Depends(),
+) -> EquipmentType:
     """
     Modifies an existing EquipmentType object
 
+    Parameters:
+        equipment_type (EquipmentType): The new EquipmentType that will replace the old one
+        id (int): The id of the EquipmentType to modify
+        subject (User): The user attempting the action
+        equipment_service (EquipmentService): The Equipment backend service layer
     Returns:
-        EquipmentType - the Equipment Type that was just added
+        EquipmentType: The Equipment Type that was just added
     """
-    ...
+    return equipment_service.modify_equipment_type(subject, id, equipment_type)
 
-@api.delete("/delete-equipment-type", tags=["Equipment Reservation System Admin"])
-def delete_equipment_type(equipment_type: EquipmentType, pid_onyen: tuple[int, str] = Depends(authenticated_pid)) -> EquipmentType:
+
+@api.delete("/delete-equipment-type", tags=["Admin Equipment Reservation System"])
+def delete_equipment_type(
+    id: int,
+    subject: User = Depends(registered_user),
+    equipment_service: EquipmentService = Depends(),
+) -> EquipmentType:
     """
     Deletes an EquipmentType object from the system
 
+    Parameters:
+        id (int): The id of the EquipmentType to delete
+        subject (User): The user attempting the action
+        equipment_service (EquipmentService): The Equipment backend service layer
     Returns:
-        EquipmentType - the Equipment Type that was deleted
+        EquipmentType: The Equipment Type that was deleted
     """
-    ...
+    return equipment_service.delete_equipment_type(subject, id)
