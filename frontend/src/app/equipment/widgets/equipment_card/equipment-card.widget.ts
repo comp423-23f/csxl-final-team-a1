28a08a7f60abb9bca8019d11b521894c56a39a29
Andrew Lockard <alockard@unc.edu>
2023-12-04 11:44:28 -0500

    Updated Equipment Card widget
    Made it so the 'Check-Out' button will be greyed out when there are no
    items available for checkout. Also added link to correct reserve screen.

--- frontend/src/app/equipment/widgets/equipment_card/equipment-card.widget.ts
+++ frontend/src/app/equipment/widgets/equipment_card/equipment-card.widget.ts
@@ -1,15 +1,24 @@
 import { Component, Input } from '@angular/core';
 import EquipmentType from '../../equipment-type.model';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'equipment-card',
-    templateUrl: './equipment-card.widget.html',
-    styleUrls: ['./equipment-card.widget.css']
+  selector: 'equipment-card',
+  templateUrl: './equipment-card.widget.html',
+  styleUrls: ['./equipment-card.widget.css']
 })
 export class EquipmentCard {
   /** Inputs and outputs go here */
   @Input() type!: EquipmentType;
 
   /** Constructor */
-  constructor() { }
+  constructor(private router: Router) {}
+
+  onClick() {
+    this.router.navigate([
+      '/equipment-reservations',
+      'reserve-screen',
+      this.type.id
+    ]);
+  }
 }
